6daae5f6e2b13021e5b94cf1e2ee92ce74434090
Vld <vlad.velikan@gmail.com>
2020-12-08 03:29:23 +0700

    Create a function "create_random_file"
    Создана функция для заполнения файла случайными целыми числами

--- Files_sorting/Files_sorting.cpp
+++ Files_sorting/Files_sorting.cpp
@@ -1,6 +1,36 @@
 ﻿#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+void create_random_file(FILE* f)
+{
+    const char* name = "f.txt";
+    int* a, n;
+    cout << "Введите количество элементов, которые хотите записать в файл: ";
+    cin >> n;
+
+    a = new int[n];
+    f = fopen(name, "wt");
+
+    if (f == NULL)
+    {
+        cout << "Проблемы при открытии файла!\n";
+    }
+    else
+        for (int i = 0; i < n; i++)
+        {
+            a[i] = rand() % 10;
+            fprintf(f, "%d ", a[i]);
+        }
+    fclose(f);
+}
 
 int main()
 {
-    
+    FILE* f, * f1, * f2, * f3;
+    const char* name1, * name2, * name3;
+    create_random_file(f);
+
+    return 0;
 }
